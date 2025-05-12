2f869893dd4549d5c7be6a29bdd2ace7d4c99be8
linke <linke@duck.com>
2025-05-12 23:05:48 +0300

    Complete Task 3

--- src/Main.java
+++ src/Main.java
@@ -13,12 +13,19 @@ public class Main {
         }
         System.out.println("Сумма трат за месяц составила " + sum + " рублей");
 
-        //task 2
+        // task 2
 
         System.out.println("Задача 2");
 
         Arrays.sort(ledger);
         System.out.println("Минимальная сумма трат за неделю составила " + ledger[0] + " рублей ");
         System.out.println("Максимальная сумма трат за неделю составила " + ledger[ledger.length - 1] + " рублей ");
+
+        // task 3
+
+        System.out.println("Задача 3");
+
+        double avgExpense = (double) sum / ledger.length;
+        System.out.println("Средняя сумма трат за месяц составила " + avgExpense + " рублей");
     }
 }
