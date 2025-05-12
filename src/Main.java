245073aeb6adadb5daf839f10d9df4d46e8c3ff7
linke <linke@duck.com>
2025-05-12 22:59:24 +0300

    Complete Task 2

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         // task 1
@@ -10,5 +12,13 @@ public class Main {
             sum += index;
         }
         System.out.println("Сумма трат за месяц составила " + sum + " рублей");
+
+        //task 2
+
+        System.out.println("Задача 2");
+
+        Arrays.sort(ledger);
+        System.out.println("Минимальная сумма трат за неделю составила " + ledger[0] + " рублей ");
+        System.out.println("Максимальная сумма трат за неделю составила " + ledger[ledger.length - 1] + " рублей ");
     }
 }
