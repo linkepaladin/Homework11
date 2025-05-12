6336b75e8a0561b85baca944e4d39e8f4ba6905e
linke <linke@duck.com>
2025-05-12 22:52:05 +0300

    Complete Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!!!");
+        // task 1
+
+        System.out.println("Задача 1");
+
+        int[] ledger = {100, 500, 200, 50, 40};
+        int sum = 0;
+        for (int index: ledger) {
+            sum += index;
+        }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
     }
 }
